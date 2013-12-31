ce629c3cd621b900dcb4ff60579da958be27b1c2
Jared Winick <jaredwinick@koverse.com>
2013-12-31 00:37:28 -0700

    proposed implementation for ACCUMULO-2113

--- src/main/java/com/koverse/ApplicationServletContextListener.java
+++ src/main/java/com/koverse/ApplicationServletContextListener.java
@@ -3,12 +3,12 @@ package com.koverse;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-
 import org.apache.accumulo.core.client.AccumuloException;
 import org.apache.accumulo.core.client.AccumuloSecurityException;
 import org.apache.accumulo.core.client.Connector;
 import org.apache.accumulo.core.client.Instance;
 import org.apache.accumulo.core.client.ZooKeeperInstance;
+import org.apache.accumulo.core.util.ClientThreads;
 
 
 public class ApplicationServletContextListener implements
@@ -19,37 +19,7 @@ public class ApplicationServletContextListener implements
 	
 	public void contextDestroyed(ServletContextEvent event) {
 		
-		try {
-			System.out.println("Closing Instance");
-			instance.close();
-			System.out.println("Instance closed...waiting");
-			
-			/*
-			 * Currently it appears we need to wait for the ZK threads (particularly the ClientCnxn SendThread)
-			 *  to shutdown before letting the container clean up the application/drop loaded classes. If we do 
-			 *  not sleep, the container will clean up the application and when the ZK ClientCnxn SendThread finally
-			 *  stops, we end up seeing a
-			 *  java.lang.NoClassDefFoundError: org/apache/zookeeper/server/ZooTrace
-			 *  as the ZK code is trying to use ZooTrace which has already been unloaded by the container. The 
-			 *  ZooKeeper.close() method isn't blocking and it doesn't appear there is any way to know when the
-			 *  internal threads have actually exited.
-			 *  
-			 *  This is the same issue a user appears to have with Tomcat
-			 *  http://mail-archives.apache.org/mod_mbox/tomcat-users/201306.mbox/%3CBAY174-W2088813852FC18913D5FC8A89B0@phx.gbl%3E
-			 *  also cross posted at
-			 *  http://mail-archives.apache.org/mod_mbox/zookeeper-user/201306.mbox/%3CBAY174-W21494DF40247669DA7B719A89B0@phx.gbl%3E
-			 */
-			try {
-				Thread.sleep(2000);
-			}
-			catch (InterruptedException ie) {
-				ie.printStackTrace();
-			}
-		}
-		catch (AccumuloException e) {
-			e.printStackTrace();
-		}
-		
+		ClientThreads.shutdownNow();
 		System.out.println("Context Destroyed");
 
 	}
---
+++ src/main/java/org/apache/accumulo/core/util/ClientThreads.java
@@ -0,0 +1,113 @@
+package org.apache.accumulo.core.util;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.zookeeper.ZooKeeper;
+
+public class ClientThreads {
+	
+	/**
+	 * kills all threads created by internal Accumulo singleton resources. After
+	 * this method is called, no accumulo client will work in the current
+	 * classloader.
+	 */
+	public static void shutdownNow() {
+		shutdownThriftTransportPoolThreads();
+		shutdownZooKeepers();
+		waitForZooKeeperClientThreads();
+	}
+	
+	/**
+	 * Forcefully shutdown the Closer Daemon of the ThriftTransportPool. If the Instance.close()
+	 * functionality gets backed out, then likely the TTP.close() will not exist either as it came 
+	 * in the same commit (https://github.com/apache/accumulo/commit/04f81b50d65b1d359cef2d38d5a6793dd6b4065f)
+	 * Additionally, this forceful method will allow users of current Accumulo versions to use these ClientThreads
+	 * methods in their own applications if need be without modification to Accumulo code.
+	 *
+	 */
+	private static void shutdownThriftTransportPoolThreads() {
+		Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+		for (Thread thread : threadSet) {
+			//System.out.println(String.format("[%s] [%s]", thread.getName(), thread.getClass().getName()));
+			if (thread.getName().equals("Thrift Connection Pool Checker")) {
+				thread.stop();
+				
+				while (thread.isAlive()) {
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Wait for ZooKeeper threads to die
+	 */
+	private static void waitForZooKeeperClientThreads() {
+		Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+		for (Thread thread : threadSet) {
+			if (thread.getClass().getName().startsWith("org.apache.zookeeper.ClientCnxn")) {
+
+				while (thread.isAlive()) {
+					System.out.println("thread " + thread.getName() + " is still alive in state: "  + thread.getState().toString());
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				System.out.println("thread " + thread.getName() + " is now dead");
+			}
+		}
+	}
+	
+	/**
+	 * Gracefully shutdown the ZooKeepers that the ZooSession holds a reference to
+	 */
+	private static void shutdownZooKeepers() {
+		
+		try {
+			Class zooSessionClass;
+			
+			// get the package private ZooSession - refactored to fate package in 1.5
+			zooSessionClass = Class.forName("org.apache.accumulo.core.zookeeper.ZooSession");
+			Field sessionsField = zooSessionClass.getDeclaredField("sessions");
+			sessionsField.setAccessible(true);
+			Map<?,?> sessions = (Map<?,?>) sessionsField.get(null);
+			
+			// Iterate over each ZooSessionInfo and get the ZooKeeper and close it
+			for (Object zsi : sessions.values()) {
+				Field zooKeeperField = zsi.getClass().getDeclaredField("zooKeeper");
+				zooKeeperField.setAccessible(true);
+				ZooKeeper zooKeeper = (ZooKeeper) zooKeeperField.get(zsi);
+				zooKeeper.close();
+			}
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
